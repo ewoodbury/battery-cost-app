888bf85b1e4247fee470e7e34acfbfddc053a7f9
Ethan Woodbury <woodburyethan60@gmail.com>
2020-05-31 14:29:26 -0700

    Initial commit - starter files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>BatteryCost.app</h1>
+      <p>An application for modeling the cost of lithium-ion batteries.</p>
+    </div>
+  );
+}
+
+export default App;
