ac0e7b791537cc3abdc591a0ac37dfc97baa0590
Ethan Woodbury <woodburyethan60@gmail.com>
2020-06-05 17:08:57 -0700

    Finished state management for sliders

--- src/scripts/CellInput.js
+++ src/scripts/CellInput.js
@@ -1,66 +1,41 @@
 import React, { useState } from "react";
 import Slider from "./Slider";
-// import cellParameters from "../data/cellParameters.js";
+import cellParameters from "../data/cellParameters.js";
 
 function CellInput() {
-  const [myState, setMyState] = useState({
-    npRatio: 1.02,
-    electrodeArea: 500,
-    catCap: 170,
-    catActiveFrac: 0.98,
-  });
-  // const [myState, setMyState] = useState({cellParameters})
+  const [myState, setMyState] = useState({ params: cellParameters });
 
   function handleChange(name) {
-    return (event) => setMyState({ ...myState, [name]: event.target.value });
+    return (event) => {
+      const updatedCellParameters = myState.params.map((param) => {
+        if (param.name === name) {
+          param.value = event.target.value;
+        }
+        return param;
+      });
+      setMyState({ params: updatedCellParameters });
+    };
   }
+
+  const parameterList = myState.params.map((param) => (
+    <Slider
+      key={param.name}
+      name={param.name}
+      label={param.label}
+      unit={param.unit}
+      value={param.value}
+      min={param.min}
+      max={param.max}
+      step={param.step}
+      handleChange={handleChange}
+    />
+  ));
 
   return (
     <div id="cell-input" className="container">
       <h3 className="card-header">Cell</h3>
       <p className="card-description">Enter cell parameters:</p>
-      <form>
-        <Slider
-          name="npRatio"
-          label="N-P Ratio"
-          value={myState.npRatio}
-          handleChange={handleChange}
-          min="0.8"
-          max="1.2"
-          step="0.01"
-          unit=""
-        />
-        <Slider
-          name="electrodeArea"
-          label="Electrode Surface Area"
-          value={myState.electrodeArea}
-          handleChange={handleChange}
-          min="0"
-          max="5000"
-          step="1"
-          unit="cm2"
-        />
-        <Slider
-          name="catCap"
-          label="Cathode Capacity"
-          value={myState.catCap}
-          handleChange={handleChange}
-          min="0"
-          max="400"
-          step="1"
-          unit="mAh/g"
-        />
-        <Slider
-          name="catActiveFrac"
-          label="Cathode Active Fraction"
-          value={myState.catActiveFrac}
-          handleChange={handleChange}
-          min="0.9"
-          max="1"
-          step=".01"
-          unit=""
-        />
-      </form>
+      <form>{parameterList}</form>
     </div>
   );
 }
