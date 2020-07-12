2b74f75159c9a6e8999f608f4d2111688f15524a
Ethan Woodbury <woodburyethan60@gmail.com>
2020-07-12 10:31:52 -0700

    Add icons to credit section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PriceInput from "./scripts/PriceInput.js";
 import Statistics from "./scripts/Statistics.js";
 import PieChart from "./scripts/PieChart.js";
 import BarChart from "./scripts/BarChart.js";
+import Footer from "./scripts/Footer.js";
 import cellParameters from "./data/cellParameters.js";
 import priceParameters from "./data/priceParameters.js";
 import calculateResults from "./scripts/calculateResults.js";
@@ -51,17 +52,7 @@ function App() {
         <Statistics results={myState.results} />
         <PieChart results={myState.results} />
         <BarChart />
-
-        <div id="credit-section">
-          <p className="credit">Created by Ethan Woodbury</p>
-          <a
-            className="github"
-            target="_blank"
-            href="https://github.com/ewoodbury/battery-cost-app"
-          >
-            Github
-          </a>
-        </div>
+        <Footer />
       </div>
     </div>
   );
