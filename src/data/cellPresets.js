a8c47dd9ae3acfe45a5e22c119932f1fa4336e8e
Ethan Woodbury <ewoodbury@tesla.com>
2020-07-26 17:28:38 -0700

    Add cellPresets and pricePresets files for preset bar graphs

--- src/data/cellPresets.js
+++ src/data/cellPresets.js
@@ -1,3 +1,127 @@
-const cellPresets = {
-    nmc811:
-}
+const cellPresets = [
+  {
+    label: "NMC 811",
+    params: [
+      {
+        name: "catCap",
+        value: 190,
+      },
+      {
+        name: "anCap",
+        value: 360,
+      },
+      {
+        name: "avgDisVoltage",
+        value: 3.75,
+      },
+      {
+        name: "catLoading",
+        value: 28,
+      },
+      {
+        name: "anLoading",
+        value: 15,
+      },
+      {
+        name: "npRatio",
+        value: 1.02,
+      },
+      {
+        name: "electrodeArea",
+        value: 500,
+      },
+      {
+        name: "catActiveFrac",
+        value: 0.98,
+      },
+      {
+        name: "anActiveFrac",
+        value: 0.98,
+      },
+    ],
+  },
+  {
+    label: "NCA 80:15:05",
+    params: [
+      {
+        name: "catCap",
+        value: 180,
+      },
+      {
+        name: "anCap",
+        value: 360,
+      },
+      {
+        name: "avgDisVoltage",
+        value: 3.7,
+      },
+      {
+        name: "catLoading",
+        value: 26,
+      },
+      {
+        name: "anLoading",
+        value: 15,
+      },
+      {
+        name: "npRatio",
+        value: 1.02,
+      },
+      {
+        name: "electrodeArea",
+        value: 500,
+      },
+      {
+        name: "catActiveFrac",
+        value: 0.98,
+      },
+      {
+        name: "anActiveFrac",
+        value: 0.98,
+      },
+    ],
+  },
+  {
+    label: "LFP",
+    params: [
+      {
+        name: "catCap",
+        value: 140,
+      },
+      {
+        name: "anCap",
+        value: 360,
+      },
+      {
+        name: "avgDisVoltage",
+        value: 3.3,
+      },
+      {
+        name: "catLoading",
+        value: 25,
+      },
+      {
+        name: "anLoading",
+        value: 15,
+      },
+      {
+        name: "npRatio",
+        value: 1.02,
+      },
+      {
+        name: "electrodeArea",
+        value: 500,
+      },
+      {
+        name: "catActiveFrac",
+        value: 0.98,
+      },
+      {
+        name: "anActiveFrac",
+        value: 0.98,
+      },
+    ],
+  },
+];
+
+export default cellPresets;
